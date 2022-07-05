2b34bb6da1fa7aef3569fa596ef8b0821ae0aa4b
Alicia Padrón <aliciaapadron@gmail.com>
2022-07-05 13:26:18 +0200

    add first exercice

---
+++ prueba.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+    public static void main(String[]args){
+       int a=10;
+       int b=30;
+       int c=20;
+      System.out.print(suma(a,b,c));
+    }
+    public static int suma(int a, int b, int c){
+       return a + b +c;
+    }
+}
